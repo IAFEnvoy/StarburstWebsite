6e2fed236508e61c1d6deb25b796a6648c4de91e
IAFEnvoy <tanksheng545@126.com>
2023-08-20 00:22:13 +0800

    update path

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,4 +1,2 @@
-window.onload = _ => {
-    if (window.location.href.indexOf('index.html') == -1 && navigator.language == 'zh-CN')
-        window.location = 'index_zh.html';
-}
+if (window.location.href.indexOf('index.html') == -1 && navigator.language == 'zh-CN')
+    window.location = 'index_zh.html';
